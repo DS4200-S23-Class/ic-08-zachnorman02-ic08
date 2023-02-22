c91abedc985f66efdba479995a56af884430ff2f
lordfundarbyrd <lordfundarbyrd@gmail.com>
2023-02-22 16:04:20 -0500

    Add in bar chart

--- main.js
+++ main.js
@@ -14,52 +14,52 @@ d3.csv('data/data.csv').then(
     (data) => {
         console.log(data);
 
+        const X_SCALE = d3
+          .scaleBand()
+          .range([0, VIS_WIDTH])
+          .domain(
+            data.map(function (d) {
+              return d.Category;
+            })
+          )
+          .padding(0.2);
+
         const MAX_Y = d3.max(data, (d) => {
           return parseInt(d.Value);
         });
 
         const Y_SCALE = d3
           .scaleLinear()
-          .domain([0, MAX_Y])
-          .range([0, VIS_HEIGHT]);
+          .domain([0, MAX_Y + 1])
+          .range([VIS_HEIGHT, 0]);
 
-        FRAME.selectAll("rect")
+        FRAME.selectAll("bars")
           .data(data)
           .enter()
           .append("rect")
-          .attr("y", (d) => {
-            return 500-Y_SCALE(parseInt(d.Value));
+          .attr("x", function (d) {
+            return X_SCALE(d.Category) + MARGINS.left;
           })
-          .attr("x", (d, i) => {
-            return (i * 20) + MARGINS.left;
+          .attr("y", function (d) {
+            return Y_SCALE(d.Value) + MARGINS.bottom;
           })
-          .attr("height", (d) => {
-            return Y_SCALE(parseInt(d.Value));
+          .attr("width", X_SCALE.bandwidth())
+          .attr("height", function (d) {
+            return VIS_HEIGHT - Y_SCALE(d.Value);
           })
-          .attr("width", 10)
           .attr("class", "bar");
 
         FRAME.append("g")
           .attr(
             "transform",
-            "translate(" + (VIS_WIDTH + MARGINS.left + MARGINS.right) + "," + MARGINS.top + ")"
-          )
-          .call(d3.axisLeft(Y_SCALE).ticks(4))
-          .attr("font-size", 20);
-        
-        FRAME.append("g")
-          .attr(
-            "transform",
             "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")"
           )
-          .call(
-            d3
-              .axisBottom(Y_SCALE)
-              .tickFormat(function (d) {
-                return d.Category;
-              })
-              .ticks(4)
-          )
-          .attr("font-size", 20);
+          .call(d3.axisBottom(X_SCALE).ticks(5))
+          .attr("font-size", 12);
+
+        FRAME.append("g")
+          .attr("transform", `translate( ${MARGINS.bottom}, ${MARGINS.left})`)
+          .call(d3.axisLeft(Y_SCALE).ticks(5))
+          .attr("font-size", 12);
     }
 )
